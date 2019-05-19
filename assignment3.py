15c293850b6a0b3829e8a341ae64785d1399b753
lukhuber <lukas.huber@protonmail.com>
2019-05-19 12:22:42 +0200

    init

--- assignment3.py
+++ assignment3.py
@@ -1,12 +1,11 @@
 #! /usr/bin/env python3
 
+import json
 import vcf
 import httplib2
 
-__author__ = 'XXX'
+__author__ = 'Lukas HUBER'
 
-
-##
 ##
 ## Aim of this assignment is to annotate the variants with various attributes
 ## We will use the API provided by "myvariant.info" - more information here: https://docs.myvariant.info
@@ -14,18 +13,18 @@ __author__ = 'XXX'
 ## 1) Annotate the first 900 variants in the VCF file
 ## 2) Store the result in a data structure (not in a database)
 ## 3) Use the data structure to answer the questions
-##
 ## 4) View the VCF in a browser
 ##
 
 class Assignment3:
     
-    def __init__(self):
+    def __init__(self, file):
         ## Check if pyvcf is installed
         print("PyVCF version: %s" % vcf.VERSION)
         
         ## Call annotate_vcf_file here
-        self.vcf_path = ""  # TODO
+        self.vcf_path = file
+        print("vcf file =", file)
 
     def annotate_vcf_file(self):
         '''
@@ -34,12 +33,7 @@ class Assignment3:
         - Store the result in a data structure
         :return:
         '''    
-        print("TODO")
-                
-        ##
-        ## Example loop
-        ##
-        
+        print("Annotating vcf file")
         ## Build the connection
         h = httplib2.Http()
         headers = {'content-type': 'application/x-www-form-urlencoded'}
@@ -60,13 +54,8 @@ class Assignment3:
         res, con = h.request('http://myvariant.info/v1/variant', 'POST', params, headers=headers)
         annotation_result = con.decode('utf-8')
         
-        ## TODO now do something with the 'annotation_result'
-        
-        ##
-        ## End example code
-        ##
-        
-        return None  ## return the data structure here
+        self.annotation_result_json = json.loads(annotation_result)
+        return self.annotation_result_json
     
     
     def get_list_of_genes(self):
@@ -74,7 +63,11 @@ class Assignment3:
         Print the name of genes in the annotation data set
         :return:
         '''
-        print("TODO")
+        print("List of genes:")
+        for i in self.annotation_result_json:
+            if 'cadd' in i:
+                if 'genename' in i['cadd']['gene']:
+                    print("     ", i['cadd']['gene']['genename'])
     
     
     def get_num_variants_modifier(self):
@@ -82,7 +75,13 @@ class Assignment3:
         Print the number of variants with putative_impact "MODIFIER"
         :return:
         '''
-        print("TODO")
+        num_variants_modifier = 0
+        for i in self.annotation_result_json:
+            if 'snpeff' in i:
+                key, value = "putative_impact", "MODIFIER"
+                if key in i['snpeff']['ann'] and value == i['snpeff']['ann']['putative_impact']:
+                    num_variants_modifier += 1
+        print("Number of variants modifier:                       ",num_variants_modifier)
         
     
     def get_num_variants_with_mutationtaster_annotation(self):
@@ -90,7 +89,12 @@ class Assignment3:
         Print the number of variants with a 'mutationtaster' annotation
         :return:
         '''
-        print("TODO")
+        variants_with_mutationtaster_annotation = 0
+        for i in self.annotation_result_json:
+            if 'dbnsfp' in i:
+                if 'mutationtaster' in i['dbnsfp']:
+                    variants_with_mutationtaster_annotation += 1
+        print("Number of variants with mutationtaster annotation: ",variants_with_mutationtaster_annotation)
         
     
     def get_num_variants_non_synonymous(self):
@@ -98,7 +102,13 @@ class Assignment3:
         Print the number of variants with 'consequence' 'NON_SYNONYMOUS'
         :return:
         '''
-        print("TODO")
+        variants_non_synonymous = 0
+        for i in self.annotation_result_json:
+            if 'cadd' in i:
+                key, value = "consequence", "NON_SYNONYMOUS"
+                if key in i['cadd'] and value == i['cadd']['consequence']:
+                    variants_non_synonymous += 1
+        print("Number of variants non synonymous:                 ", variants_non_synonymous)
         
     
     def view_vcf_in_browser(self):
@@ -114,12 +124,18 @@ class Assignment3:
     
     def print_summary(self):
         self.annotate_vcf_file()
-        print("Print all results here")
+        print("\n---------------------- RESULTS ----------------------")
+        self.get_list_of_genes()
+        self.get_num_variants_modifier()
+        self.get_num_variants_with_mutationtaster_annotation()
+        self.get_num_variants_non_synonymous()
+        print("-----------------------------------------------------\n")
     
     
 def main():
+    file = "chr16.vcf"
     print("Assignment 3")
-    assignment3 = Assignment3()
+    assignment3 = Assignment3(file)
     assignment3.print_summary()
     print("Done with assignment 3")
         
